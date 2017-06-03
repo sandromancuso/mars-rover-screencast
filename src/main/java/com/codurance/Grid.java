91a08df5fdac699be3c19492cc6a85c41ac8ca6b
Sandro Mancuso <sandromancuso@yahoo.com>
2017-06-03 22:28:40 +0200

    Removed magical number. Now using maxLength and maxHeight.

--- src/main/java/com/codurance/Grid.java
+++ src/main/java/com/codurance/Grid.java
@@ -3,27 +3,26 @@ package com.codurance;
 import static com.codurance.Direction.*;
 
 class Grid {
-    private final int maxHeigh;
-    private final int maxLenght;
+    private final int maxHeight;
+    private final int maxLength;
 
-    Grid(int maxHeigh, int maxLenght) {
-        this.maxHeigh = maxHeigh;
-        this.maxLenght = maxLenght;
+    Grid(int maxHeight, int maxLength) {
+        this.maxHeight = maxHeight;
+        this.maxLength = maxLength;
     }
 
     Coordinate nextCellPosition(Coordinate coordinate, Direction direction) {
         int y = coordinate.y();
         int x = coordinate.x();
         if (direction == NORTH) {
-            y = (y + 1) % 10;
+            y = (y + 1) % maxHeight;
         } else if (direction == SOUTH){
-            y = (y == 0) ? 9 : y - 1;
+            y = (y == 0) ? maxHeight - 1 : y - 1;
         } else if (direction == EAST) {
-            x = (x + 1) % 10;
+            x = (x + 1) % maxLength;
         } else if (direction == WEST) {
-            x = (x == 0) ? 9 : x - 1;
+            x = (x == 0) ? maxLength - 1 : x - 1;
         }
         return new Coordinate(x, y);
-
     }
 }
